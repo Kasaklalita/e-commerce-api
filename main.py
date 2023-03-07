36e3f979652ee0d4d069c8895465d814e6f9b70e
Veniamin Polienko <kasaklalita@gmail.com>
2023-03-07 19:13:09 +0300

    Added cors origins

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ import database
 from routers import authentication, user, product, business
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(database.engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(business.router)
